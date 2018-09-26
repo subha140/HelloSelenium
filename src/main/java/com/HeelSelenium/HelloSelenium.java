f243de8c5d55b3279e77e5285019e3428b75d6c7
Dell <Dell@DESKTOP-6B3GB9B>
2018-09-26 10:44:02 +0530

    commit done!!!

---
+++ src/main/java/com/HeelSelenium/HelloSelenium.java
@@ -0,0 +1,11 @@
+package com.HeelSelenium;
+
+public class HelloSelenium {
+
+	public static void main(String[] args) 
+	{
+		 System.out.println( "Hello Selenium" );
+
+	}
+
+}
---
+++ src/test/java/com/HelloSeleniumTest/Demo.java
@@ -0,0 +1,13 @@
+package com.HelloSeleniumTest;
+
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void a()
+	{
+		System.out.println("HELLO SELI");
+	}
+
+}
